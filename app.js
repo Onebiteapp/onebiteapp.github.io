3f515fa7eaa0daf9e90f4c59dd3e197b2150cbd6
Onebiteapp <dasgourab40602@gmail.com>
2026-02-26 19:23:49 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB63wz4Z8maGyYk7UdvZk2IvxwGXYePisA",
+  authDomain: "onebiteapp-b19c3.firebaseapp.com",
+  databaseURL: "https://onebiteapp-b19c3-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "onebiteapp-b19c3",
+  storageBucket: "onebiteapp-b19c3.firebasestorage.app",
+  messagingSenderId: "925416277184",
+  appId: "1:925416277184:web:e987b2a512221cebea5925",
