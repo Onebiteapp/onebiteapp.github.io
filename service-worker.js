41c8b13e85694f77e8fa2501acef84f04d001e4a
Onebiteapp <dasgourab40602@gmail.com>
2026-02-28 15:29:01 +0530

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('onebite-store').then(function(cache) {
-      return cache.addAll([
-        '/',
-      ]);
-    })
-  );
-});
