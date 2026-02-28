2f4b4b4132a91fc3cf85f38a6025e82291763a45
Onebiteapp <dasgourab40602@gmail.com>
2026-02-28 15:57:52 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,21 @@
 self.addEventListener('install', function(event) {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.open('onebite-v1').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
